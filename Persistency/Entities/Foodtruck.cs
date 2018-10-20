4076ae75b4a0bab87b093379e18e4e0d277e662a
Mirosław Gałczyński <contact@miroslawgalczynski.com>
2018-10-20 20:27:31 +0200

    Persitance project

---
+++ Persistency/Entities/Foodtruck.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Persistency.Entities
+{
+    internal class Foodtruck
+    {
+        internal Guid Id { get; set; }
+        internal string Name { get; set; }
+    }
+}
