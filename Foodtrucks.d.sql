342904911972b30930225324558870384108d7b0
Mirosław Gałczyński <contact@miroslawgalczynski.com>
2019-01-22 11:19:52 +0100

    Dump changed

--- Foodtrucks.d.sql
+++ Foodtrucks.d.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fc141c09daf31154d80830cdf42e0a431c586fd2bc7f80c22d7dfd38a90b3bf
+oid sha256:d30fbbca392f6f0f2ca4d7fcf67b2b9a34514de5b0e7edd0cbace9cfb67969cd
 size 3548867
