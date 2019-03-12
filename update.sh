62ca3b4fcd55116563339c28f0fef4222a79f470
Mirosław Gałczyński <miroslaw.galczynski@nokia.com>
2019-03-12 17:12:57 +0100

    Post implementation documentation

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-compose -f development.yml -p foodtrucker down --remove-orphans
+docker-compose -f development.yml -p foodtrucker down --remove-orphans --rmi all -v
 docker-compose -f development.yml -p foodtrucker pull
 docker-compose -f development.yml -p foodtrucker up -d
