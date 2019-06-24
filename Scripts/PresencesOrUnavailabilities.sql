9a744d668af841e2e9fadc151c75e4489aaec740
Mirosław Gałczyński <contact@miroslawgalczynski.com>
2019-06-24 16:58:45 +0200

    Changes in init SQL scripts

--- Scripts/PresencesOrUnavailabilities.sql
+++ Scripts/PresencesOrUnavailabilities.sql
@@ -1,11 +1,11 @@
 BEGIN TRANSACTION;
 
-UPDATE "PresencesOrUnavailabilities" SET "StartTime" = DATE_TRUNC('day', NOW()), "EndTime" = DATE_TRUNC('day', NOW()) + INTERVAL '1' DAY WHERE "EndTime" IS NOT NULL;
+UPDATE "PresencesOrUnavailabilities" SET "StartTime" = DATE_TRUNC('day', NOW()), "EndTime" = DATE_TRUNC('day', NOW()) + INTERVAL '4' DAY WHERE "EndTime" IS NOT NULL;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 INSERT INTO "PresencesOrUnavailabilities"("Id", "FoodtruckId", "StartTime", "EndTime", "Title", "Description", "Location")
-SELECT UUID_GENERATE_V4(), "FoodtruckId", "EndTime", "EndTime" + INTERVAL '1 day', "Title", "Description", NULL
+SELECT UUID_GENERATE_V4(), "FoodtruckId", "EndTime", "EndTime" + INTERVAL '3 day', "Title", "Description", NULL
 FROM "PresencesOrUnavailabilities"
 WHERE "EndTime" IS NOT NULL;
 
